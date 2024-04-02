74b51cb9515990a4a7e342d85cd13bd671d52a68
tomegoodson <158308016+tomegoodson@users.noreply.github.com>
2024-04-02 14:21:43 -0500

    statesevents.tg

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState(false);
+
+  const handleCartToggle = () => {
+    setInCart(!inCart);
+  };
+
+  const buttonText = inCart ? "Remove From Cart" : "Add to Cart";
+  const itemClass = inCart ? "in-cart" : "";
+
   return (
-    <li className="">
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleCartToggle}>{buttonText}</button>
     </li>
   );
 }
